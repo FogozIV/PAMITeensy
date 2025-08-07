849a466863821873f0a2e0226eb73b0b0faca84b
FogozIV <fogoziv@gmail.com>
2025-08-07 13:11:42 +0200

    SPSA Methodo not done at all but some things have higher priority

--- include/controller/calibration_methodo/SPSAMethodo.h
+++ include/controller/calibration_methodo/SPSAMethodo.h
@@ -5,8 +5,29 @@
 #ifndef PAMITEENSY_SPSAMETHODO_H
 #define PAMITEENSY_SPSAMETHODO_H
 
+#include <controller/calibration_methodo/BaseCalibrationMethodo.h>
+#include <vector>
+#include <controller/calibration_methodo/ESCType.h>
 
-class SPSAMethodo {
+class SPSAMethodo : public CalibrationMethodo{
+    std::vector<double> gainsValues;
+    std::vector<double> logGains;
+    std::vector<int8_t> perturbation;
+    ESCType::ESC typeOfControl;
+public:
+    SPSAMethodo(std::shared_ptr<BaseRobot> robot, std::shared_ptr<Mutex> sdMutex, ESCType::ESC typeOfControl);
+
+    void save() override;
+
+    void printStatus(Stream &stream) override;
+
+    void start() override;
+
+    void stop() override;
+
+    void launchStage();
+
+    void cleanupStage();
 };
 
 
--- src/controller/calibration_methodo/SPSAMethodo.cpp
+++ src/controller/calibration_methodo/SPSAMethodo.cpp
@@ -2,4 +2,32 @@
 // Created by fogoz on 06/08/2025.
 //
 
-#include "../../../include/controller/calibration_methodo/SPSAMethodo.h"
+#include <controller/calibration_methodo/SPSAMethodo.h>
+
+SPSAMethodo::SPSAMethodo(std::shared_ptr<BaseRobot> robot, std::shared_ptr<Mutex> sdMutex, ESCType::ESC typeOfControl) : CalibrationMethodo(robot, sdMutex), typeOfControl(typeOfControl) {
+}
+
+void SPSAMethodo::save() {
+}
+
+void SPSAMethodo::printStatus(Stream &stream) {
+}
+
+void SPSAMethodo::start() {
+}
+
+void SPSAMethodo::stop() {
+}
+
+void SPSAMethodo::launchStage() {
+    logGains.clear();
+    gainsValues = robot->getController()->getGains(typeOfControl);
+    for (auto gain : gainsValues) {
+        logGains.push_back(log(gain));
+    }
+    random(0, 2);
+
+}
+
+void SPSAMethodo::cleanupStage() {
+}
